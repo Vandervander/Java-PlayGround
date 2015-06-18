1646f57bf442ef450341bd8d8b010ec80aabc562
Vandervander <bobbungii@gmail.com>
2015-06-18 14:38:10 +1200

    Add inheritance.java
    Explore some simple inheritance, by extending a class

---
+++ inheritance.java
@@ -0,0 +1,50 @@
+/**
+ * Class Foo documentation
+ * @author Dylan
+ *
+ */
+class SuperBar {
+	/** Constructor with no arguments */
+	public SuperBar() {this(6);}
+	/** Constructor taking an int for i*/
+	public SuperBar(int i) {this.i = i;}
+	
+	private int i;
+	static int j = 42;
+	
+	public void setI(int i) {this.i = i;}
+	public int getI() {return i;}
+	public int getJ() {return j;}
+}
+
+/**
+ * Class Bar documentation
+ * @author Dylan
+ *
+ */
+class Bar extends SuperBar{
+	/** Constructor using superClass */
+	public Bar() {super(); i = 9;}
+	
+	private int i;
+	
+	public void setJ(int j) {this.j = j;}
+	public int getBarI() {return i;}
+}
+
+public class inheritance {
+	public static void main(String [] arg){
+		//Make new SuperBar and Bar objects
+		SuperBar superBar = new SuperBar(10);
+		Bar bar = new Bar();
+		//i tests
+		System.out.println("SuperBar.i = "+superBar.getI());
+		System.out.println("Bar.getBarI = "+bar.getBarI());
+		System.out.println("bar.getI = "+bar.getI()+"\n");
+		//j tests
+		System.out.println("Bar.getJ = "+bar.getJ()+" SuperBar.getJ = "+superBar.getJ());
+		System.out.println("Use Bar to reset j to 13");
+		bar.setJ(13);
+		System.out.println("Bar.getJ = "+bar.getJ()+" SuperBar.getJ = "+superBar.getJ());
+	}
+}
